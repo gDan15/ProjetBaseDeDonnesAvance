763d29f3c51e4a27a0ac2fd67ff1d040becfdbb4
gaetan guru <gaetan.guru@hotmail.com>
2017-12-18 01:59:03 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+import java.util.Arrays;
+import java.util.Iterator;
+import org.bson.Document;
+
+public class Main {
+	public static void main(String [] args) {
+		
+	}
+}
