711cdea4a10293e76fcb6ee970587b97b47f4023
gaetan guru <gaetan.guru@hotmail.com>
2017-12-18 02:03:17 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,40 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.MongoCredential;
 import com.mongodb.MongoClientOptions;
+
+
 import java.util.Arrays;
 import java.util.Iterator;
-import org.bson.Document;
 
+import org.bson.Document;
 public class Main {
 	public static void main(String [] args) {
+	    MongoClientOptions.Builder options = MongoClientOptions.builder().sslEnabled(true);
+	    
+	    // Creating Credentials 
+//	    char[] password= {'D','@','n','e','l','e','c','t','r','o','5','9','M','i'};
+//	    MongoCredential credential = MongoCredential.createCredential("gDan","Test",password);
+	    
+	    //mongodb+srv://gDan:<PASSWORD>@cluster0-tgfhl.mongodb.net/test
+	    MongoClientURI uri = new MongoClientURI("mongodb+srv://gDan:LT0NSa3V8PfkkYnp@cluster0-tgfhl.mongodb.net/Test");
+
+	    MongoClient mongoClient = new MongoClient(uri);
+//	    DB db = mongoClient.getDB( "Test" );
 		
+	    System.out.println("Connected to the database successfully");  
+	    // Accessing the database 
+	    MongoDatabase database = mongoClient.getDatabase("Test");  
+	    // Retrieving a collection 
+	    MongoCollection<Document> collection = database.getCollection("Test");
+	    System.out.println("Collection sampleCollection selected successfully"); 
+	    // Getting the iterable object 
+	    FindIterable<Document> iterDoc = collection.find();
+	    int i = 1; 
+        // Getting the iterator 
+        Iterator it = iterDoc.iterator(); 
+        while(it.hasNext()) {
+        	System.out.println(it.next());
+        	i++;
+        }
 	}
 }
