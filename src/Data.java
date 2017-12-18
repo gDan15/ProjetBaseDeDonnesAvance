4a719180bb6a064e0361b295e439a7868d3699f8
gaetan guru <gaetan.guru@hotmail.com>
2017-12-18 23:50:34 +0100

    add field figured out

--- src/Data.java
+++ src/Data.java
@@ -1,6 +1,9 @@
+import com.mongodb.DBCursor;
+
 //package data;
 public class Data {
 	public void addColumn(String column, Object value) {
+		
 		
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
@@ -8,33 +13,64 @@ import java.util.Iterator;
 import org.bson.Document;
 public class Main {
 	public static void main(String [] args) {
-//	    MongoClientOptions.Builder options = MongoClientOptions.builder().sslEnabled(true);
-	    
-	    // Creating Credentials 
-//	    char[] password= {};
-//	    MongoCredential credential = MongoCredential.createCredential("gDan","Test",password);
-	    
-	    //mongodb+srv://gDan:<PASSWORD>@cluster0-tgfhl.mongodb.net/test
-	    MongoClientURI uri = new MongoClientURI("mongodb+srv://gDan:LT0NSa3V8PfkkYnp@cluster0-tgfhl.mongodb.net/Test");
+////	    MongoClientOptions.Builder options = MongoClientOptions.builder().sslEnabled(true);
+//	    
+//	    // Creating Credentials 
+////	    char[] password= {};
+////	    MongoCredential credential = MongoCredential.createCredential("gDan","Test",password);
+//	    
+//	    //mongodb+srv://gDan:<PASSWORD>@cluster0-tgfhl.mongodb.net/test
+//	    MongoClientURI uri = new MongoClientURI("mongodb+srv://gDan:LT0NSa3V8PfkkYnp@cluster0-tgfhl.mongodb.net/Test");
+//
+//	    MongoClient mongoClient = new MongoClient(uri);
+////	    DB db = mongoClient.getDB( "Test" );
+//		
+//	    System.out.println("Connected to the database successfully");  
+//	    // Accessing the database 
+//	    MongoDatabase database = mongoClient.getDatabase("Test");  
+//	    // Retrieving a collection 
+//	    MongoCollection<Document> collection = database.getCollection("Test");
+//	    System.out.println("Collection sampleCollection selected successfully"); 
+////	    Getting the iterable object 
+//	    FindIterable<Document> iterDoc = collection.find();
+////	    DBCursor cursor = collection.find();
+//	    int i = 1; 
+//        // Getting the iterator 
+//        Iterator it = iterDoc.iterator();
+//        while(it.hasNext()) {
+//        	System.out.println(it.next());
+//        	i++;
+//        }
+		DBCursor curs;
+		MongoClientURI uri = new MongoClientURI("mongodb+srv://gDan:LT0NSa3V8PfkkYnp@cluster0-tgfhl.mongodb.net/Test");
+		MongoClient mongoClient = new MongoClient(uri);
+		DB db = mongoClient.getDB("Test");
+		DBCollection coll = db.getCollection("Test");
+		curs= coll.find();
+		int i = 1; 
+		while(i<4) {
+			BasicDBObject p = (BasicDBObject) curs.next();
+			BasicDBObject q = p;
+			BasicDBObject sold = new BasicDBObject("sold", false);
 
-	    MongoClient mongoClient = new MongoClient(uri);
-//	    DB db = mongoClient.getDB( "Test" );
-		
-	    System.out.println("Connected to the database successfully");  
-	    // Accessing the database 
-	    MongoDatabase database = mongoClient.getDatabase("Test");  
-	    // Retrieving a collection 
-	    MongoCollection<Document> collection = database.getCollection("Test");
-	    System.out.println("Collection sampleCollection selected successfully"); 
-	    // Getting the iterable object 
-	    FindIterable<Document> iterDoc = collection.find();
-	    int i = 1; 
-        // Getting the iterator 
-        Iterator it = iterDoc.iterator();
-        while(it.hasNext()) {
-        	System.out.println(it.next());
-        	i++;
-        }
-        mongoClient.close();
+//			o.put("sold", false);
+//			coll.update(p.append("sold",false),q);
+//			System.out.println(p);
+//			System.out.println(q);
+			BasicDBObject newDocument =
+					new BasicDBObject().append("$set",
+					new BasicDBObject().append("total clients", 99));
+			System.out.println(newDocument);
+			coll.update(p, newDocument);
+			i++;
+		}
+		curs= coll.find();
+		while(i<7) {
+			BasicDBObject o = (BasicDBObject) curs.next();
+//			o.append("sold",false);
+			System.out.println(o);
+			i++;
+		}
+		mongoClient.close();
 	}
 }
